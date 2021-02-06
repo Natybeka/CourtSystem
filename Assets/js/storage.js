37d8a6098b3fc8c6f4306ec1a879019176160657
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-02-06 14:03:18 +0300

    User Object Designed

---
+++ Assets/js/storage.js
@@ -0,0 +1,8 @@
+//Objects Design
+
+// User Object has a name (Early design may be subject to change)
+function User(userName, accessType, password){
+    this.userName = userName;
+    this.accessType = accessType;
+    this.password = password; 
+}
