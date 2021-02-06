fda02043a830d5085697ceb0adb52640221e11e7
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-02-06 17:12:40 +0300

    Case Object Designed

--- Assets/js/storage.js
+++ Assets/js/storage.js
@@ -1,8 +1,51 @@
 //Objects Design
 
-// User Object has a name (Early design may be subject to change)
+// User Object has a name,access and password (Early design may be subject to change)
 function User(userName, accessType, password){
     this.userName = userName;
     this.accessType = accessType;
     this.password = password; 
+}
+
+/*  
+    Case Object design must have:
+    1. ID or a case number,
+    2. status, active or closed
+    3. plaintiff,
+    4. defendant
+    5. courtLevel (level at which the case is currently being heard)
+    6. judge
+    7. caseOpened date at which case has been opened
+    8. description for the case
+    9. nextCourtDate
+    
+    and finally methods
+    changing courtLevel and status
+*/
+function Case(caseID, status, plaintiff, defendant, courtLevel, judge, caseOpened, description, nextCourtDate) {
+    this.caseID = caseID;
+    this.status = status;
+    this.plaintiff = plaintiff;
+    this.defendant = defendant;
+    this.courtLevel = courtLevel;
+    this.caseOpened = caseOpened;
+    this.description = description;
+    this.judge = judge;
+    this.nextCourtDate = nextCourtDate;
+    
+    this.changeStatus = function(newStatus) {
+        if (newStatus != this.status) {
+            this.status = newStatus;
+        }    
+    }
+    
+    this.changeCourtLevel = function(newCourtLevel) {
+        if (this.courtLevel != newCourtLevel) {
+            this.courtLevel = newCourtLevel;
+        }
+    }
+
+    this.changeJudge = function(newJudge) {
+        this.judge = newJudge;
+    }
 }
